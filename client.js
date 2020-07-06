6db18b705bd6478f8d7306a17c34f5790748553b
NobuokaYu <nobu0202yu@gmail.com>
2020-07-06 15:55:30 +0900

    fin

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ $(function() {
   var socket = io.connect('http://localhost:3000');
   var $input = $('input[name="message"]');
   var $list = $('ul');
-  
+
   // テキストを元にチャットエリアにメッセージを追加する
   function createItem(text) {
     var $li = $('<li>').text(text);
@@ -13,10 +13,10 @@ $(function() {
   $('form').submit(function(e) {
     e.preventDefault();
     var text = $input.val();
-  
+
     socket.emit('text', text);
     $input.val('');
-  
+
     // 自分自身が送信したテキストはサーバーから受信しないのでここで
     // メッセージの追加を行う
     createItem(text);
